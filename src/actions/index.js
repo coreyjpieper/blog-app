d7d1981e27998db90a1f68d2da5fabb2dced570f
Corey <coreyjpieper@gmail.com>
2019-06-07 23:30:27 -0500

    Made action creator and reducer for FETCH_USER

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,9 @@ export const fetchPosts = () => async function(dispatch, getState) {
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
+
+export const fetchUser = (id) => async function(dispatch, getState) {
+  const response = await jsonplaceholder.get(`/users/${id}`);
+
+  dispatch({ type: 'FETCH_USER', payload: response.data })
+};
