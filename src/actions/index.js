7d86d561f8810be3dee8d22fa9d0fbc7654cbaac
Corey <coreyjpieper@gmail.com>
2019-06-16 22:29:47 -0500

    Added fetchPostAndUsers action creator to prevent overfetching

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,18 +1,21 @@
 import jsonplaceholder from '../apis/jsonplaceholder';
 import _ from 'lodash';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 export const fetchPosts = () => async function(dispatch, getState) {
   const response = await jsonplaceholder.get('/posts');
 
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => function(dispatch, getState) {
-  _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async function(dispatch, getState) {
   const response = await jsonplaceholder.get(`/users/${id}`);
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-});
+};
